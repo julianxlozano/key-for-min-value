3de33781c9edfcd50016fdd05769760957c4a1eb
julianxlozano <julianxlozano@gmail.com>
2021-02-24 14:34:45 -0600

    commit tree 23f4d965d91b63d9c9b3c561a3ae0c65302c707a

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,36 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require 'pry'
+
 
 def key_for_min_value(name_hash)
 
+    
+    
+
+    def smallest(array)
+        min=array[0]
+        array.each do |num|
+            if num<min
+               # binding.pry
+                min=num
+            end
+               
+            
+        end
+        return min 
+    end    
+      
+    
+   new_values= name_hash.collect do |key,value|
+        value 
+    end
+
+    name_hash.invert[smallest(new_values)]
+
+  
+  
+    
+
 end
+
